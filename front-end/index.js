296d19520498c478da4bafb2bce5b756534d666f
duccodon <ntduc22@clc.fitus.edu.vn>
2024-11-10 15:57:09 +0700

    Update js for repsponsive button and fix some responsive CSS

--- front-end/index.js
+++ front-end/index.js
@@ -1,19 +1,24 @@
 const menuItems = document.querySelectorAll('.menu-item');
 const sidebarFunctionButton = document.getElementById('sidebar-function');
 const functionPopup = document.getElementById('sidebar-functionPopup');
-
+const topFunctionPopup = document.getElementById("top-functionPopup");
 
 menuItems.forEach(item => {
     item.addEventListener('click', (e) => {
         if (item.id != "sidebar-function") {
-            functionPopup.style.display = 'none';
-            menuItems.forEach(i => i.classList.remove('active'));
-            item.classList.add('active');
+            if (item.id == "top-function") {
+              if (topFunctionPopup.style.display == "block")
+                topFunctionPopup.style.display = "none";
+              else topFunctionPopup.style.display = "block";
+            } else {
+              functionPopup.style.display = "none";
+              menuItems.forEach((i) => i.classList.remove("active"));
+              item.classList.add("active");
+            }
         } else {
-            if (functionPopup.style.display == 'block')
-                functionPopup.style.display = 'none';
-            else
-                functionPopup.style.display = 'block';
+          if (functionPopup.style.display == "block")
+            functionPopup.style.display = "none";
+          else functionPopup.style.display = "block";
         }
     });
 });
@@ -22,6 +27,12 @@ menuItems.forEach(item => {
 document.addEventListener('click', (e) => {
     if (!functionPopup.contains(e.target) && !sidebarFunctionButton.contains(e.target)) {
         functionPopup.style.display = 'none';
+    }
+});
+
+document.addEventListener('click', (e) => {
+    if (!topFunctionPopup.contains(e.target) && !document.getElementById('top-function').contains(e.target)) {
+        topFunctionPopup.style.display = 'none';
     }
 });
 
