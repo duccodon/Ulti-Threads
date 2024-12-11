a1f8d118dbca1fd30cc405ce24e9b1514b29b30e
duccodon <ntduc22@clc.fitus.edu.vn>
2024-12-11 17:15:21 +0700

    Fix minor details in homepage

--- routes/pageRouter.js
+++ routes/pageRouter.js
@@ -1,9 +1,10 @@
 const router = require("express").Router();
 const { showHomepage } = require('../controller/pageController');
-
+const { showPostDetails } = require('../controller/pageController');
 // router.use('/', init);
 // router.get('/', showList);
 // router.get('/:id', showDetails);
 router.get('/', showHomepage);
+router.get('/Post', showPostDetails);
 
 module.exports = router;
--- routes/profileRouter.js
+++ routes/profileRouter.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 const { showProfile } = require('../controller/pageController');
-const { showPostDetails } = require('../controller/pageController');
+
 // router.use('/', init);
 // router.get('/', showList);
 // router.get('/:id', showDetails);
 router.get('/', showProfile);
-router.get('/Post', showPostDetails);
+
 module.exports = router;
